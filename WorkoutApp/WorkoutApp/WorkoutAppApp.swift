e22ab08501c3ec63c6232ce983d6c3578266296f
Aidan Fields <89093189+AidanFi@users.noreply.github.com>
2024-08-24 14:42:38 -0500

    Add files via upload

---
+++ WorkoutApp/WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Aidan Fields on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
