074fae931b47ed58c2f4c815c951b23530c4518f
Aidan Fields <aidanfields12@gmail.com>
2024-08-27 14:30:29 -0500

    Added Progress Circle

--- WorkoutApp/WorkoutApp/HomeView.swift
+++ WorkoutApp/WorkoutApp/HomeView.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var calories: Int = 123
+    @State var active: Int = 123
+    @State var stand: Int = 123
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    VStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Calories")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.red)
+                            
+                            Text("123 kcal")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Active")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.green)
+                            
+                            Text("52 minutes")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Stand")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.blue)
+                            
+                            Text("8 hours")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                    }
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        ProgressCircleView(progress: $calories, goal: 200, color: .red)
+                        
+                        ProgressCircleView(progress: $calories, goal: 200, color: .green)
+                            .padding(.all, 20)
+                        
+                        ProgressCircleView(progress: $calories, goal: 200, color: .blue)
+                            .padding(.all, 40)
+                    
+                    }
+                    .padding(.horizontal)
+                    
+                    Spacer()
+                }
+                .padding()
+            }
+        }
     }
 }
 
